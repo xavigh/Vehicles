d5b5ccebbd3f9e94e068eb3030638231e59bdda7
xavigk <fgargallo@gmail.com>
2019-05-14 12:02:14 +0200

    3
    3

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,8 @@
-"use strict";
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
     car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerHTML = "Car Number Plate: " + car.plate
+    var info = document.getElementById('carInfo');
+    info.innerHTML = "Car Number Plate: " + car.plate
         + "</br> COLOR: " + car.color + "</br> BRAND: " + brand
         + " WHEELS: " + JSON.stringify(car.wheels);
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -2,7 +2,10 @@
 function createCar(plate: string, brand: string, color: string) {
     let car = new Car(plate, color, brand);
     car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerHTML= "Car Number Plate: " + car.plate
+    var info = document.getElementById('carInfo');
+    
+    
+    info.innerHTML = "Car Number Plate: " + car.plate
         + "</br> COLOR: " + car.color + "</br> BRAND: " + brand
         + " WHEELS: " + JSON.stringify(car.wheels);
 }
